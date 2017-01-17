cefc2f6703bf859d93bce9909dd91b7cca6b7539
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-17 21:18:17 +0800

    [update] test for create table and insert table

--- src/bwsl/db/mysql/DBAdminTest.java
+++ src/bwsl/db/mysql/DBAdminTest.java
@@ -2,6 +2,7 @@ package bwsl.db.mysql;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DBAdminTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -11,6 +12,19 @@ public class DBAdminTest {
 		
 		ResultSet rs = dba.selectAll("test");
 		while(rs.next()) System.out.println(rs.getString("P") + " " + rs.getString("A") + " " + rs.getString("B"));
+		
+		ArrayList<CloumnParameter> paras = new ArrayList<CloumnParameter>();
+		paras.add(new CloumnParameter("A", "int"));
+		paras.add(new CloumnParameter("B", "int"));
+		dba.create("todrop", paras);
+		
+		ArrayList<String> values = new ArrayList<String>();
+		values.add("INSERT");
+		values.add("GOOD");
+		dba.insert("todrop", values);
+		
+		rs = dba.selectAll("todrop");
+		while(rs.next()) System.out.println(rs.getString("A") + " " + rs.getString("B"));
 		
 		dba.drop("todrop");
 		
