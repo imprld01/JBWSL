ed9f67bc2f9ad03bc387f7065e45426384f95059
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-17 19:19:21 +0800

    [update] advoid the condition taht unicode=false but encoding is not null

--- src/bwsl/db/mysql/DBAdmin.java
+++ src/bwsl/db/mysql/DBAdmin.java
@@ -31,20 +31,12 @@ public class DBAdmin {
 		this.prpareUrl(addr, dbName, true, encoding);
 	}
 	
-	public DBAdmin(String addr, String dbName, boolean unicode, String encoding) throws ClassNotFoundException, SQLException {
+	public DBAdmin(String addr, String dbName, String encoding, String user, String pwd) throws ClassNotFoundException, SQLException {
 		
 		System.out.println("---------- MySQL JDBC Connection ----------");
 		System.out.println("Try Connecting Database...");
 		
-		this.prpareUrl(addr, dbName, unicode, encoding);
-	}
-	
-	public DBAdmin(String addr, String dbName, boolean unicode, String encoding, String user, String pwd) throws ClassNotFoundException, SQLException {
-		
-		System.out.println("---------- MySQL JDBC Connection ----------");
-		System.out.println("Try Connecting Database...");
-		
-		this.prpareUrl(addr, dbName, unicode, encoding);
+		this.prpareUrl(addr, dbName, true, encoding);
 		this.connect(user, pwd);
 	}
 	
