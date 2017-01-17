2685ca1b000a10c1bab0a9baa20590856510df79
Bo-Wun Shih <bowns1688@hotmail.com>
2017-01-17 20:52:49 +0800

    [update] preparing testing for prepareAll()

--- src/bwsl/db/mysql/DBAdminTest.java
+++ src/bwsl/db/mysql/DBAdminTest.java
@@ -1,8 +1,15 @@
 package bwsl.db.mysql;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBAdminTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
+		DBAdmin dba = new DBAdmin(DBAdmin._ADDR_LOCALHOST_PORT_3306, "test");
+		dba.connect("root", "5868");
 		
+		ResultSet rs = dba.selectAll("test");
+		while(rs.next()) System.out.println(rs.getString("P") + " " + rs.getString("A") + " " + rs.getString("B"));
 	}
 }
